539a9bfd9584eddc2258a0e34072136878699051
Jon Washington <jonathan.washington@gmail.com>
2018-08-12 15:45:49 -0700

    successfully used innerText to build lists of cookie estimates

--- scripts.js
+++ scripts.js
@@ -22,23 +22,53 @@ var hours = [
   '10am: ', '11am: ', '12am: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ',
 ]
 
-Shoppe.prototype.buildList = function(){
+
+//below I've copied the original buildlist function and am updating it to create a node on the DOM and add the list items that way.
+
+Shoppe.prototype.writeList = function(){
   var list = document.getElementById(this.identification);
   var dailyTotal = 0;
   for (var index = 0; index < hours.length; index++){
     var hour = hours[index];
     var cookies = this.randomHourlyCookies();
-    list.innerHTML += '<li>'+ hour +'   ' + cookies +'</li>';
+    console.log(cookies);
+    var listItem = document.createElement('li');
+    console.log(listItem);
+    listItem.innerText = (hour + ' ' + cookies);
+    list.appendChild(listItem);
+    console.log(listItem);
     dailyTotal += cookies;
   }
-  list.innerHTML += '<li>Total: '+ dailyTotal + '</li>';
+  var finalTotal = document.createElement('li');
+  finalTotal.innerText = ('Total: '+ dailyTotal);
+  list.appendChild(finalTotal);
 }
 
-PioneerSquareShoppe.buildList();
-PortlandAirportShoppe.buildList();
-WashingtonSquareShoppe.buildList();
-SellwoodShoppe.buildList();
-PearlDistrictShoppe.buildList();
+PioneerSquareShoppe.writeList();
+PortlandAirportShoppe.writeList();
+WashingtonSquareShoppe.writeList();
+SellwoodShoppe.writeList();
+PearlDistrictShoppe.writeList();
+//end of updated function.
+
+/*the following function is commented out for now. It is the untouched buildlist function.
+Shoppe.prototype.buildList = function(){
+var list = document.getElementById(this.identification);
+var dailyTotal = 0;
+for (var index = 0; index < hours.length; index++){
+var hour = hours[index];
+var cookies = this.randomHourlyCookies();
+list.innerHTML += '<li>'+ hour +'   ' + cookies +'</li>';
+dailyTotal += cookies;
+}
+list.innerHTML += '<li>Total: '+ dailyTotal + '</li>';
+} */
+
+//PioneerSquareShoppe.buildList();
+//PortlandAirportShoppe.buildList();
+//WashingtonSquareShoppe.buildList();
+//SellwoodShoppe.buildList();
+//PearlDistrictShoppe.buildList();
 
 console.log(PioneerSquareShoppe);
 console.log(PioneerSquareShoppe.randomHourlyCookies());
