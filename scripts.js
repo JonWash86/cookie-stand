e934e4f4d0560b7d95c4dd3268e5e6ddb9510d0f
Jon Washington <jonathan.washington@gmail.com>
2018-08-14 20:32:18 -0700

    end of day commit

--- scripts.js
+++ scripts.js
@@ -4,12 +4,12 @@ var Shoppe = function(shoppeName, minCustomers, maxCustomers, averageCookies, id
   this.maxCustomers = maxCustomers;
   this.averageCookies = averageCookies;
   this.identification = identification;
+  this.randomHourlyCookies = function(){
+    var thisHour = Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers;
+    return Math.floor(thisHour * this.averageCookies);
+  }
 }
 
-Shoppe.prototype.randomHourlyCookies = function(){
-  var thisHour = Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers;
-  return Math.floor(thisHour * this.averageCookies);
-}
 
 var PioneerSquareShoppe = new Shoppe("Pioneer Square", 17, 88, 5.2, 'pioneer','pioneerTable');
 var PortlandAirportShoppe = new Shoppe("Portland Airport", 6, 24, 1.2, 'airport');
@@ -21,7 +21,6 @@ var hours = [
   '10am: ', '11am: ', '12am: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ',
 ]
 
-//write a function which writes the table heading to the proper area.
 var shoppes = [
   PioneerSquareShoppe,
   PortlandAirportShoppe,
@@ -36,77 +35,37 @@ Shoppe.prototype.writeTableHeading = function(){
   var row = document.createElement('tr');
   var cell = document.createElement('td');
   var heading = document.createElement('th');
+  var dailyTotal = 0;
+  table.setAttribute('id', 'columns');
   heading.innerText = this.shoppeName;
+  heading.setAttribute('colspan','2');
   table.appendChild(heading);
   div.appendChild(table);
-  /*for (var index = 0; index < hours.length; index++){
+  for (var index = 0; index < hours.length; index++){
+    var cell = document.createElement('td');
+    var row = document.createElement('tr');
     var hour = hours[index];
     var cookies = this.randomHourlyCookies();
     cell.innerText = hour;
+    console.log(cell);
     row.appendChild(cell);
+    console.log(row);
     cell = document.createElement('td');
     cell.innerText = cookies;
+    console.log(cell);
     row.appendChild(cell);
+    console.log(row);
     table.appendChild(row);
-
-  };*/
+    dailyTotal += cookies;
+  };
+  cell = document.createElement('td');
+  cell.innerText = ('Total: ' + dailyTotal);
+  cell.setAttribute('colspan', '2');
+  table.appendChild(cell);
 }
-PioneerSquareShoppe.writeTableHeading(tableReference);
 
 var tableReference = document.getElementById('salesTables');
 
 for (var index = 0; index < shoppes.length; index++){
   shoppes[index].writeTableHeading(tableReference);
 }
-
-
-
-//below is the new, in-progress function to write the table.
-
-
-/*
-//The following is the commented-out original function, which wrote a list.
-
-Shoppe.prototype.writeList = function(){
-  var list = document.getElementById(this.identification);
-  var dailyTotal = 0;
-  for (var index = 0; index < hours.length; index++){
-    var hour = hours[index];
-    var cookies = this.randomHourlyCookies();
-    console.log(cookies);
-    var listItem = document.createElement('li');
-    console.log(listItem);
-    listItem.innerText = (hour + ' ' + cookies);
-    list.appendChild(listItem);
-    console.log(listItem);
-    dailyTotal += cookies;
-  }
-  var finalTotal = document.createElement('li');
-  finalTotal.innerText = ('Total: '+ dailyTotal);
-  list.appendChild(finalTotal);
-}
-
-PioneerSquareShoppe.writeList();
-PortlandAirportShoppe.writeList();
-WashingtonSquareShoppe.writeList();
-SellwoodShoppe.writeList();
-PearlDistrictShoppe.writeList();
-
-
-
-console.log(PioneerSquareShoppe);
-console.log(PioneerSquareShoppe.randomHourlyCookies());
-
-console.log(PortlandAirportShoppe);
-console.log(PortlandAirportShoppe.randomHourlyCookies());
-
-console.log(WashingtonSquareShoppe);
-console.log(WashingtonSquareShoppe.randomHourlyCookies());
-
-console.log(SellwoodShoppe);
-console.log(SellwoodShoppe.randomHourlyCookies());
-
-console.log(PearlDistrictShoppe);
-console.log(PearlDistrictShoppe.randomHourlyCookies());
-
-*/
